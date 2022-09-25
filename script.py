9c1ee77573134448698d5f23081632d5b5bb8b82
Damian <damianduquel@gmail.com>
2022-09-25 12:43:07 -0500

    Correccion id's de las Observaciones

--- script.py
+++ script.py
@@ -1,24 +1,53 @@
+from datetime import datetime
 import os
 from os import mkdir
 import cv2
 import keyboard
 
-id = 0
+print("Presione Espacio o Enter para tomas fotos")
 
 while True:
+
+    now = datetime.now()
+    dia = format(now.day)
+    mes = format(now.month)
+    anio = format(now.year)
+    hora = format(now.hour)
+    minuto = format(now.minute)
+    segundo = format(now.second)
     
     if keyboard.is_pressed('e'):
             break
 
     if keyboard.is_pressed('space') or keyboard.is_pressed('enter'):
         
+        if int(dia) < 10:
+            dia = '0' + dia
+
+        if int(mes) < 10:
+            mes = '0' + mes
+
+        anio = anio[2:]
+
+        if int(hora) < 10:
+            hora = '0' + hora
+
+        if int(minuto) < 10:
+            minuto = '0' + minuto
+
+        if int(segundo) < 10:
+            segundo = '0' + segundo
+
+
         print("Tomando fotos")
 
-        archivo = 'observacion_'
-        nombre_carpeta = archivo + str(id)
 
-        mkdir(nombre_carpeta)        
-        id = id + 1
+        id = dia + mes + anio + hora + minuto + segundo
+
+        archivo = 'O_'
+        nombre_carpeta = archivo + str(id)
+        
+        mkdir(nombre_carpeta)
         
         ruta = os.path.abspath(nombre_carpeta)
 
